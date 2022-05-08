0e6b4d5baef8788ed0c2e8da51545429e2f79d67
Hazique Quazi <hazique>
2022-05-08 16:24:24 -0400

    Manage Cookies

--- ab-test-mab-ui/src/App.js
+++ ab-test-mab-ui/src/App.js
@@ -1,21 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
 import { useEffect, useState } from 'react';
+import { useCookies } from 'react-cookie';
 
 function App() {
 
   var [time, setTime] = useState(0);
+  const [cookies, setCookie] = useCookies(['rl_subid', 'rl_config']);
 
   useEffect(() => {
 
-    const fetch_time = async () => {
+    const interval = setInterval( async () => {
       const response = await fetch('http://127.0.0.1:5000/');
       const data = await response.json();
       setTime(data.time);
-    };
+    }, 1000);
 
-    fetch_time()
-  }, [time]);
+    if (cookies.rl_subid || cookies.rl_config){
+      // Handle case where the user has already completed the Test
+      console.log("debug cookies here");
+    }
+    else{
+      console.log("Cookie not found");
+    }
+
+
+    // useEffect may return "cleanup function"
+    return () => clearInterval(interval);
+  }, []);
 
   return (
     <div className="App">
